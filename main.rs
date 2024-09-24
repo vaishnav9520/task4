31033bfb2f863214e8fafe8eaadcf04642bfcf47
vaishnav9520 <vaishnavp9520@gmail.com>
2024-09-24 11:41:57 +0530

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,11 @@
+use chrono::Local;
+
+fn main() {
+    let name = "<vaishnav>"; // Replace <Your Name> with your actual name
+    let current_time = Local::now();
+    println!("Hello {}, right now the time is {}", name, current_time);
+}
+
+
+   
+
---
+++ time.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set('asia/india'); // Set your timezone
+$name = "<vaishnav>"; // Replace <Your Name> with your actual name
+echo "Hello $name, right now the time is " . date('Y-m-d H:i:s');
+?>
